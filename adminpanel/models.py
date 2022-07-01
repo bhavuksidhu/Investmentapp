694702e9486b3c23958a4fb60cc6d1cc80e40fdd
Rahul Kumar <rah.kr97@gmail.com>
2022-07-01 08:44:16 +0530

    templates

--- adminpanel/models.py
+++ adminpanel/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class ContactData(models.Model):
+    company_address = models.TextField()
+    company_number = models.CharField(max_length=20, unique=True, null=True)
+
 # Create your models here.
+class StaticData(models.Model):
+    about_us = models.TextField()
+    privacy_policy = models.TextField()
+    terms_and_conditions = models.TextField() 
+    contact_data = models.OneToOneField(ContactData,on_delete=models.SET_NULL,null=True)
+
+class FAQ(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
