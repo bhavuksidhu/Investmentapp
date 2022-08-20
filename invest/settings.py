94b3b397ade620a03c3ea19b34901793268941fe
Rahul Kumar <rah.kr97@gmail.com>
2022-08-20 20:40:43 +0530

    hehe

--- invest/settings.py
+++ invest/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "api.middleware.DenyInactiveUser"
 ]
 
 ROOT_URLCONF = "invest.urls"
