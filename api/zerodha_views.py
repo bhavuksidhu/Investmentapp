2dabad73d3761563613c3f1cb11f555b82e4da88
Rahul Kumar <rah.kr97@gmail.com>
2022-09-06 13:23:07 +0530

    hehe

--- api/zerodha_views.py
+++ api/zerodha_views.py
@@ -87,8 +87,8 @@ class Redirect(APIView):
                     data["created_at"] = timezone.now()
 
                     with open("postbacks/data.json","w",encoding="utf-8") as f:
-                        json.dump(data,f,ensure_ascii=False)
-                        
+                        json.dump(data,f,ensure_ascii=False,default=str)
+
                     ZerodhaData.objects.create(**data)
 
                     result = "Success"
