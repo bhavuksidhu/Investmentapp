66fe9cb89b33c54021d4de0368da6b6a8b4eca8a
Otuoma Sanya <otuoma@users.noreply.github.com>
2022-11-22 19:39:18 +0300

    changed firebase_token

--- core/models.py
+++ core/models.py
@@ -48,7 +48,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(max_length=255, unique=True, null=True)
     is_email_verified = models.BooleanField(default=False)
-    firebase_token = models.TextField(default="", unique=True)
+    # firebase_token = models.TextField(default="", unique=True)
+    firebase_token = models.TextField(unique=True, null=True, blank=True)
     phone_number = models.CharField(max_length=20, unique=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
